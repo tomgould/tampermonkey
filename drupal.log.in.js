29ab4f6b6655a35ca14b0ce8443c963995973b07
tgould <tom.gould@turner.com>
2014-09-12 15:04:53 +0100

    A drupal login script

--- drupal.log.in.js
+++ drupal.log.in.js
@@ -3,15 +3,43 @@
 // @namespace    http://drupal.log.in/
 // @version      0.1
 // @description  Loggs you in to your drupal site if you're on the user page 
-// @match        http://yoursite.co.uk/user
-// @copyright    2012+, You
+// @copyright    2014+, Tom Gould
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
-var your_drupal_login = function () {
-    $('input[name="name"]').val('username');
-    $('input[name="pass"]').val('password');
-    $('#edit-submit').click();
-} 
+var your_drupal_login = function() {
+  var hostname = window.location.hostname.toLowerCase().replace('www.', '');
+  var pathname = window.location.pathname.toLowerCase();
+    
+  /**
+   * Sites to log in to
+   */
+  var drupalSites = {
+    tomgould: {
+      host: "tom-gould.co.uk",
+      user: "Username",
+      pass: "password"
+    },
+    drupalOrg: {
+      host: "drupal.org",
+      user: "Username",
+      pass: "password"
+    },
+  };
+
+  if (pathname === "/user") {
+    for (var key in drupalSites) {
+      var site = drupalSites[key];
+      if (site.host === hostname && $('input[name="name"]').length) {
+        var r = confirm("Do you want to log in as " + site.user + " using password " + site.pass + "?");
+        if (r == true) {
+            $('input[name="name"]').val(site.user);
+            $('input[name="pass"]').val(site.pass);
+            $('#edit-submit').click();
+        } 
+      }
+    }
+  }
+}
 
 your_drupal_login();
