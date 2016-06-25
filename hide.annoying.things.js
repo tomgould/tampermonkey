91aee284bcdd711ec9477e3142a926ee5779a4b0
tom.gould <tom@dealmobile.co.uk>
2016-06-25 17:55:14 +0000

    Hides all the brexit EU bullshit from facebook

--- hide.annoying.things.js
+++ hide.annoying.things.js
@@ -1,61 +1,66 @@
-  // ==UserScript==
-  // @name         Hide annoying things form your FaceBook wall
-  // @namespace    http://hide.annoying.things/
-  // @version      0.1
-  // @description  Deleted DOM nodes from facebook containing the strings in the array
-  // @match        https://www.facebook.com/
-  // @copyright    2014+, Tom Gould
-  // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-  // @grant        GM_addStyle
-  // ==/UserScript==
-  var hideAnnoyingThingsStrings = [
-    "cameron",
-    "xbox",
-    "playstation",
-    "football",
-    "poker",
-    "game",
-    "Apple",
-    "iPhone",
-    "iPad",
-    "iWatch",
-    "trees",
-    "artisanal",
-    "chutes",
-    "paying tax",
-    "coding standards",
-    "fruit vendors",
-    "information",
-    "weekdays",
-    "The Riddler",
-    "commits",
-    "text editors"
-  ];
+// ==UserScript==
+// @name         Hide annoying things form your FaceBook wall
+// @namespace    http://hide.annoying.things/
+// @version      0.1
+// @description  Deleted DOM nodes from facebook containing the strings in the array
+// @match        https://www.facebook.com/
+// @copyright    2014+, Tom Gould
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @grant        GM_addStyle
+// ==/UserScript==
+var hideAnnoyingThingsStrings = [
+  "cameron",
+  "Boris",
+  "EU ",
+  "Europe",
+  "Brexit",
+  "referendum",
+  "xbox",
+  "playstation",
+  "football",
+  "poker",
+  "game",
+  "Apple",
+  "iPhone",
+  "iPad",
+  "iWatch",
+  "trees",
+  "artisanal",
+  "chutes",
+  "paying tax",
+  "coding standards",
+  "fruit vendors",
+  "information",
+  "weekdays",
+  "The Riddler",
+  "commits",
+  "text editors"
+];
 
-  var hideAnnoyingThings = function() {
-      $('._4ikz').each(function() {
-          var elements = $(this).find('div.mbm');
-          $.each(elements, function(key, value) {
-              var findings = hideAnnoyingThings_str_contains(
-                      $(value).html(), hideAnnoyingThingsStrings, 0);
-              if (findings !== false) {
-                  $(this).remove();
-                  console.log('Found a ' + findings);
-              }
-          });
-      });
-  }
-
-  var hideAnnoyingThings_str_contains = function(haystack, needles, offset) {
-      for (i = 0; i < needles.length; i++) {
-          var str = needles[i].toLowerCase();
-          var n = (haystack.toLowerCase() + '')
-              .indexOf(str, (offset || 0));
-          if (n > -1) {
-              return str;
-          }
+var hideAnnoyingThings = function () {
+  $('._4ikz').each(function () {
+    var elements = $(this).find('div.mbm');
+    $.each(elements, function (key, value) {
+      var findings = hideAnnoyingThings_str_contains(
+        $(value).html(), hideAnnoyingThingsStrings, 0);
+      if (findings !== false) {
+        $(this).remove();
+        console.log('Found a ' + findings);
       }
-      return false;
-  }
+    });
+  });
+}
 
-  window.setInterval(hideAnnoyingThings, 1000);
+var hideAnnoyingThings_str_contains = function (haystack, needles, offset) {
+  for (i = 0; i < needles.length; i++) {
+    var str = needles[i].toLowerCase();
+    var n = (haystack.toLowerCase() + '')
+      .indexOf(str, (offset || 0));
+    if (n > -1) {
+      return str;
+    }
+  }
+  return false;
+}
+
+window.setInterval(hideAnnoyingThings, 1000);
